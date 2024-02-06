93acf56f875e7bb94c32ee57dfc4ff80b8215883
sumedhsph <sumedh.sph@gmail.com>
2024-02-06 18:22:52 +0530

    error el

--- src/components/index.js
+++ src/components/index.js
@@ -7,4 +7,5 @@ export { default as Filters } from "./Filters";
 export { default as PaginationContainer } from "./PaginationContainer";
 export { default as ProductsContainer } from "./ProductsContainer";
 export { default as Loading } from "./Loading";
+export { default as ErrorElement } from "./ErrorElement";
  
