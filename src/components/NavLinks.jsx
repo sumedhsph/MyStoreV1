684c65b6a036d610747b51525602d5fd5b2c9e1c
sumedhsph <sumedh.sph@gmail.com>
2024-02-07 18:29:02 +0530

    hide protected page links

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 function NavLinks() {
   const links = [
@@ -9,10 +10,14 @@ function NavLinks() {
     { id: 5, url: "checkout", text: "checkout" },
     { id: 6, url: "orders", text: "orders" }
   ];
+
+  const user = useSelector((state)=>state.userState.user);
+
   return (
     <>
       {links.map((link) => {
         const { id, url, text } = link;
+        if((url === "checkout" || url === "orders") && !user) return null;
         return (
           <li key={id}>
             <NavLink className="capitalize" to={url}>
